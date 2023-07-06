c81ddc3cda9518a4177af551d22c94a2cf4edb60
lumpenop <swk5373@gmail.com>
2023-07-06 19:08:19 +0900

    feat: header nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-
-
+/** @jsxImportSource @emotion/react */
+import { jsx } from "@emotion/react";
+import Header from "src/components/Header";
 
 function App() {
-
   return (
     <>
       <div>
+        <Header />
         <div>hi</div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
