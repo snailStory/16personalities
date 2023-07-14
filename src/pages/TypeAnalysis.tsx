9ee976c08a4a4fdc8264bbc6d7d8a3983bcef95d
lumpenop <swk5373@gmail.com>
2023-07-15 04:07:14 +0900

    feat: 유형별 성격 페이지 유형별 페이지 이동 버튼 추가

--- src/pages/TypeAnalysis.tsx
+++ src/pages/TypeAnalysis.tsx
@@ -1,11 +1,43 @@
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 
+const mbtis = [
+  "ENTP",
+  "INFP",
+  "ENFP",
+  "INTP",
+  "ENTJ",
+  "INTJ",
+  "ESTP",
+  "ISTP",
+  "ENFJ",
+  "INFJ",
+  "ISFP",
+  "ESFP",
+  "ISFJ",
+  "ESFJ",
+  "ISTJ",
+  "ESTJ",
+];
 function TypeAnalysis() {
   const params = useParams();
+  const navigate = useNavigate();
   const { mbti } = params;
   return (
     <div>
       <h1>성격유형</h1>
+      {!mbti && (
+        <ul>
+          {mbtis.map((item, index) => {
+            return (
+              <li key={index}>
+                <button onClick={() => navigate(`/typeAnalysis/${item}`)}>
+                  {item}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      )}
       {mbti && <div>{mbti}</div>}
     </div>
   );
