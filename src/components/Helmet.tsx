0709d21f3be21a906ceed074df1bd1dfbe6ef6c9
lumpenop <swk5373@gmail.com>
2023-08-28 20:52:40 +0900

    add: favicon

--- src/components/Helmet.tsx
+++ src/components/Helmet.tsx
@@ -7,6 +7,12 @@ const SEO = ({ title }: Props) => {
   return (
     <Helmet>
       <title>{title} - 동물 mbti</title>
+      <meta
+        name="description"
+        content="mbti 동물 테스트를 해보세요"
+      />
+      <meta name="author" content="snail story" />
+      <meta name="keywords" content="mbti, 동물 mbti, 동물, mbti 테스트, mbti 동물" />
     </Helmet>
   );
 };
